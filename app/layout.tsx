425e896769be8123460397c967b9266ac6a91d3a
codegod <ashutheola@gmail.com>
2026-04-04 20:16:03 +0100

    Delete searchagencies component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/navbar";
-import Search from "@/components/searchagencies";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,15 +25,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en">
-      <body  
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-       <div className="min-h-full flex flex-col">
-        <Header/>
-        <Search/>
-        {children}
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      >
+        <div className="min-h-full flex flex-col">
+          <Header/>
+          {children}
         </div>
       </body>
     </html>
