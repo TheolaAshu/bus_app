e33836f9573e510cd697f0f7a3e1066fb6cc0173
codegod <ashutheola@gmail.com>
2026-04-04 20:10:04 +0100

    Add searchagencies component

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <div className="bg-black">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify between h-16">
+        <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
             <div className="flex items-center flex-shrink-o">
               <Image
